232f48136f290281058228ff31dfd543154d367d
yetabalij <yetabalij@gmail.com>
2021-06-10 15:57:17 +0300

    app.js file is modiffied by adding MainContariner component it to it.

--- react-admin-dashboard/src/App.js
+++ react-admin-dashboard/src/App.js
@@ -1,9 +1,10 @@
 import { Topbar } from "./components/topbar/Topbar";
-
+import { MainContainer } from "./components/container/MainContainer";
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <MainContainer />
     </div>
   );
 }
